599803a4f4e40271dba77a3d3ee725c5bf21ba16
wasi300 <wasi300@mikadukisan.info>
2013-09-13 01:58:11 +0900

    shuuseisita

--- leapyear.php
+++ leapyear.php
@@ -5,11 +5,9 @@
 	$input = trim(fgets(STDIN));
 
 
-	if($input%4 == 0 && $input%100 !==0)
+	if($input%4 == 0 && $input%100 !==0 || $input%400 == 0)
 	{
 				echo $input."は閏年です。".PHP_EOL;
-	} else if($input%400 == 0) {
-				echo $input."は閏年です".PHP_EOL;
 	} else {
 				echo $input."は閏年ではないです。".PHP_EOL;
   }
