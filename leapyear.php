6111470082102b3a44e17162fd12a1a07a99d368
wasi300 <wasi300@mikadukisan.info>
2013-09-13 01:45:52 +0900

    learyear las

--- leapyear.php
+++ leapyear.php
@@ -1,20 +1,17 @@
 <?php
 
+
+	echo "数字をいれてください。 >>";
 	$input = trim(fgets(STDIN));
 
-	if($input%4 ===0)
+
+	if($input%4 == 0 && $input%100 !==0)
 	{
-		echo $input." is Leap Year".PHP_EOL;
-	} else if($input%100 == 0){
-			if($input%100 !== 0)
-			{
 				echo $input."は閏年です。".PHP_EOL;
-			}
 	} else if($input%400 == 0) {
 				echo $input."は閏年です".PHP_EOL;
 	} else {
 				echo $input."は閏年ではないです。".PHP_EOL;
-
   }
 
 ?>
