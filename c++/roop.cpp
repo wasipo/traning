1e97900885b5ff5c89eaef239342981935151bfb
wasi300 <wasi300@mikadukisan.info>
2013-09-26 10:59:18 +0900

    roop

---
+++ c++/roop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+
+	int i;
+
+	for(i=0; i<10; i++)
+	{
+		printf("Hello World!%3d",i);
+		cout << "Hello World!" << endl;
+	}
+
+}
