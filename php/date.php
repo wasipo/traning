04ab1759b545c1730fc1e6e2caa786bfce5e4a12
wasi300 <wasi300@mikadukisan.info>
2013-10-19 01:13:21 +0900

    dateきらい

--- php/date.php
+++ php/date.php
@@ -9,32 +9,36 @@ $week = array("日","月","火","水","木","金","土");
 $weekno = date("w",strtotime($_POST["input"]));
 $next = 7-$weekno;
 
-$i = $weekno;
-$y = 1;
-
+(int)$i = $weekno;
+$y = 0;
 
 while(1)
 {	
-		$days[-$i] = date("Y/m/d",strtotime("$input -$i day"));
-//		$tmp = date("w",strtotime("$input -$i day"));
-//		echo $week[$tmp]."<br />";
-		if($i == 0)
+
+		if($i !== $weekno)
 		{
-			while($y<$next)
+			echo date("Y/m/d",strtotime("$input -$i day")); 
+			$tmp = date("w",strtotime("$input -$i day"));
+			echo $week[$tmp]."<br />";
+		} else if($i == 0) { //0の無限ループ回避
+			for($j=6; $j >= 0; $j--)
 			{
-				$days[$y] = date("Y/m/d",strtotime("$input $y day"));
-//				$tmp = date("w",strtotime("$input $y day"));
-//				echo $week[$tmp]."<Br />";
+					$tmp = date("w",strtotime("$input -$j day"));
+					echo date("Y/m/d",strtotime("$input -$j day")).$week[$tmp]."<br />";
+			}
+			break;
+		}
+
+		if($i == 1)
+		{
+			while($y<=$next)
+			{	
+				echo date("Y/m/d",strtotime("$input $y day"));
+				$tmp = date("w",strtotime("$input $y day"));
+				echo $week[$tmp]."<Br />";
 				$y++;
 			}
 			break;
 		}
 		$i--;
-}
-
-$count = 0;
-foreach($days as $key => $val)
-{
-		echo $val.$week[$count]."<Br />";
-		$count++;
 }
