96369068e83d3872ffd90e30c81d8364357410ed
wasi300 <wasi300@mikadukisan.info>
2013-10-21 14:56:06 +0900

    a

--- php/system.php
+++ php/system.php
@@ -12,6 +12,7 @@ $inputdate = $_POST["input_date"];
 $inputmonth = $_POST["input_month"];
 $inputkubun = $_POST["input_kubun"];
 */
+
 echo "日付を入力してください。>> ";
 $inputdate = trim(fgets(STDIN));
 echo "何ヵ月後かを入力してください >> ";
@@ -19,58 +20,84 @@ $inputmonth = trim(fgets(STDIN));
 echo "指定月後の-1は(1)、指定月後の月末は(0)を入力してください >> ";
 $inputkubun = trim(fgets(STDIN));
 
-$week = array("日","月","火","水","木","金","土");
 
-//何ヶ月後のunixタイムスタンプ取得
-//取得後、年、月、日を別々の変数に保存
-//そこから日付のフォーマットxxxx/xx/xxを作成
-//$add_date = strtotime("+$inputmonth month",strtotime($inputdate));
-
+//入力値をunixタイムスタンプに変換
 $inputdate = strtotime($inputdate);
-$iYear = date('Y',$inputdate);
-$iMonth = date('m',$inputdate);
-$iDay = date('d',$inputdate);
+//入力値を年、月、日に分離 
+list($iYear,$iMonth,$iDay) = array(date('Y',$inputdate),date('m',$inputdate),date('d',$inputdate));
 
+
+/*
+* mkadd関数
+* 年、月、日、何ヵ月後かを送る
+* 入力値の計算処理をする
+* unixタイムスタンプが戻る
+*/
 
 function mkadd($y,$m,$d,$data)
 {
-
+	/*
+	*  $CalcMonth
+	*　月と何ヵ月後かを足した値
+	*/
 	$CalcMonth = $data+$m;
 
 	if($CalcMonth > 12)
 	{
+
+		/*
+		*	$SetYear
+		*	12以上だったら、12で割った値の小数点を省いた後、
+		*	その数と入力された年度の値を足す。念のため最後にintにする
+		*/
+
 		$SetYear = ($CalcMonth / 12);
 		$SetYear = round($SetYear,0,PHP_ROUND_HALF_DOWN);
 		$SetYear = $y+$SetYear;
 		$SetYear = intval($SetYear);
+	
+		/*
+		* $SetMonth 
+		* $CalcMonthを割った時の余りを月として扱う
+		*/
 		$SetMonth = ($CalcMonth % 12);
-		$confirmdate = $SetYear."/".$SetMonth."/".$d;
 
-		if(!checkdate($SetMonth,$d,$SetYear))
+
+		//日付があるか無いかを判断して、入力値から条件に応じて数を引く
+
+		if(!checkdate($SetMonth,$d,$SetYear) && $SetMonth == "2") 
 		{
-			$d = $d-1;
+				//日付が存在せず、2月だった場合にうるう年か判断する
+				if($SetYear%4==0 && $SetYear%100 !==0 || $SetYear%400 == 0)
+				{
+						//検索された月がうるう年で入力された日付が31だった場合、2を引く、30だった場合１を引く
+						if($d == 31){ $d = $d-2; } else if($d == 30){ $d = $d-1; }
+				} else {
+						if($d == 31){ $d = $d-3; } else if($d == 30){ $d = $d-2; } else if($d == 29) { $d = $d-1; }
+				}
+		} else if(!checkdate($SetMonth,$d,$SetYear)){
+				$d = $d-1;	
 		}
 
+		//計算結果のunixタイムスタンプを返す
 		return mktime(0,0,0,$SetMonth,$d,$SetYear);
 	} else {
+
+		//$CalcMonthの値が12より低い場合、それをそのまま月としてunixタイムスタンプを返す
 		return mktime(0,0,0,$CalcMonth,$d,$y);
 	}
 
 }
 
+
 $add_date = mkadd($iYear,$iMonth,$iDay,$inputmonth);
-var_dump(date("Y-m-d",$add_date));
-exit;
 
-
-
-/*
 $year = date("Y",$add_date);
 $month = date("m",$add_date);
 $day = date("d",$add_date);
 $firstday = $year."/".$month."/01";
 $inputdate = $year."/".$month."/".$day;
-*/
+
 
 //区分の条件分岐
 //1ならx月後の-1日0ならx月後の月末。
