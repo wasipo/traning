febd4f0894857c598aecc1d780f8ed6f58e50273
wasi300 <wasi300@mikadukisan.info>
2013-10-21 22:15:20 +0900

    kousin

--- php/wed.php
+++ php/wed.php
@@ -13,9 +13,9 @@ $Wno = 0;
 while($count <= $meres)
 {
 	//割り切れたらWnoが増える
-	if($count%7 === 0)
+	if($count%7 === 0) 
 	{
-		(int)$Wno++;
+			(int)$Wno++;
 	}
 
 	//3回Wnoがカウントアップしたら
@@ -24,14 +24,20 @@ while($count <= $meres)
 		//その週の日付と曜日を出力
 		foreach(range(1,12) as $mon)
 		{
-			$mi = date("Y/m/d D",mktime(0,0,0,$mon,$count,date("Y")));
+			$md = date("D",mktime(0,0,0,$mon,1,date("Y")));
+			if($md == "Thu")
+			{
+				$rcount = $count+7;
+				$mi = date("Y/m/d D",mktime(0,0,0,$mon,$rcount,date("Y")));
+			} else {
+				$mi = date("Y/m/d D",mktime(0,0,0,$mon,$count,date("Y")));
+			}
 			//配列に分割
 			$mi = explode(" ",$mi);
 			//1番目にアクセス
 			if($mi[1] === "Wed")
-			{
-	
-					echo $mi[0].$mi[1]."　水曜見っけ".PHP_EOL;
+			{	
+					echo $mi[0]." ".$mi[1].PHP_EOL;
 			}
 		}
 
@@ -40,26 +46,29 @@ while($count <= $meres)
 	$count++;
 }
 
-
-
-//やってみたけどできませんでした・・・。
-//間違ってたら教えてください！
-
+echo "===============================================".PHP_EOL;
 
 $mr = array();
 
-for($i=0; $i<12; $i++)
+for($i=1; $i<13; $i++)
 {
 	//1日の週番号取得
 	$md = date("Ymd w",mktime(0,0,0,$i,1,date("Y")));
 	//分割
 	$mi = explode(" ",$md);
-	//最初の週の曜日番号+21
-	$mr[$i] = $mi[1]+21;
+	//最初の週の曜日番号
+	if($mi[1] <= 3 || $mi[1] === "0")
+	{
+		$Wno = 3;
+		$mr[$i] = (4-$mi[1])+7*$Wno;
+	} else {
+		$Wno = 4;
+		$mr[$i] = (4-$mi[1])+7*$Wno;
+	}
 }
 
 //日付取得。mktimeで日にちを21日足します。
-for($i = 0; $i<12; $i++)
+for($i = 1; $i<13; $i++)
 {
 	$mt = date("Y/m/d D",mktime(0,0,0,$i,$mr[$i],date("Y")));
 	echo $mt.PHP_EOL;
